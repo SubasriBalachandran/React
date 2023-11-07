79576683c2af3e5e400b0ff0a5352ccb8d675fdb
SubasriBalachandran <727722euit187@skcet.ac.in>
2023-11-07 11:39:30 +0530

    Intial Commit

---
+++ src/ListElements.jsx
@@ -0,0 +1,14 @@
+import {Message} from "./Message";
+const ListElements = () =>
+{ return (
+<div>
+    <Message></Message>
+        <ul>
+        <li>red</li>
+        <li>blue</li>
+        <li>yellow</li></ul>
+        </div>
+);
+}
+
+export {ListElements};  // default exports
---
+++ src/Message.jsx
@@ -0,0 +1,22 @@
+//jsx=>javascript xml
+
+
+//addTwoNumbers
+
+function Message()
+{
+    return (
+        <div>
+            <h1>From Message.jsx</h1>
+            <h2>Hello !</h2>
+
+        </div>
+    )
+}
+
+function add()
+{
+    return 1+2;
+}
+
+export { Message ,add };  //named exports
